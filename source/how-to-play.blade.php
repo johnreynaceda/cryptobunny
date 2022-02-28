2831bf8bd017d0f604a5f08c0c7737b90e73f47b
johnreynaceda <johnreynaceda@sksu.edu.ph>
2022-02-28 14:31:24 +0800

    how-to-play

---
+++ source/how-to-play.blade.php
@@ -0,0 +1,118 @@
+@extends('_layouts.main')
+
+@section('body')
+<div class="relative bg-black pt-16 sm:pt-24 lg:pt-0">
+    <div class="mx-auto max-w-md px-4 text-center sm:px-6 py-5 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
+     <div class="flex py-5 text-white flex-col">
+        <h1 class="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-400">HOW TO PLAY THE GAME?</h1>
+        <p class="text-lg mt-3 text-justify indent-10 ">CryptoBunny  has a very unique ecosystem that allows players to enter any race, the  cost is 150 Vittles per race,
+            Gameplay is very competitive, however the rewards are still lucrative. Players compete against 9 other racers in an attempt to earn a top 3 placement. All players inside the top 3 will earn 3000Carrots for the champion, 2400Carrots for the 2nd place, 1500Carrots for the 3rd place, and a consolation prize of 1000Carrots for the 4th and 9th place.  Carrot is the platforms utility token. Knowledge, strategy and skill are all required to place in the top 3. Be prepared to race, dedicate time and be one with your cryptobunny, the rewards will make it all worthwhile.</p>
+     </div>
+     <div class="flex py-2">
+         <div class="bg-gray-50 flex flex-col items-start text-white p-2 px-6 rounded-md bg-opacity-20">
+             <h1 class="font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-400">TOKEN:</h1>
+             <h1 class="font-bold text-lg">1000 Carrot = 1000 Vittles</h1>
+             <h1 class="font-bold text-lg">1000 Carrot = 1000 Vittles</h1>
+         </div>
+     </div>
+     <div class="flex py-2 ">
+         <div class="bg-gray-50 flex flex-col items-start text-white p-2 px-6 rounded-md bg-opacity-20">
+             <h1 class="font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-400">CRYPTOBUNNY SKILLS:</h1>
+             <h1 class="font-bold text-lg">'Brawn</h1>
+             <h1 class="font-bold text-lg">'Sleight</h1>
+             <h1 class="font-bold text-lg">'Savvy</h1>
+             <h1 class="font-bold text-lg">'Grit</h1>
+         </div>
+     </div>
+     <div class="flex py-2 ">
+         <div class="bg-gray-50 flex flex-col items-start text-white p-2 px-6 rounded-md bg-opacity-20">
+             <h1 class="font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-400">CRYPTOBUNNY RARITY:</h1>
+             <h1 class="font-bold text-lg">'Guardian</h1>
+             <h1 class="font-bold text-lg">'Legend</h1>
+             <h1 class="font-bold text-lg">'Mythic</h1>
+             <h1 class="font-bold text-lg">'Immortal</h1>
+             <h1 class="font-bold text-lg">'God</h1>
+         </div>
+     </div>
+     <div class="flex py-2 ">
+         <div class="bg-gray-50 flex flex-col items-start text-white p-2 px-6 rounded-md bg-opacity-20">
+             <h1 class="font-bold text-lg  ">Number of Race depends on the Rarity og the Bunny</h1>
+             <h1 class="text-left">Guardian-450 Vittles (maximum of 3 race per day)</h1>
+             <h1 class="text-left">Legend -600 Vittles (maximum of 4 race per day)</h1>
+             <h1 class="text-left">Mythic -750 Vittles (maximum of 5 race per day)</h1>
+             <h1 class="text-left">Immortal -750 Vittles (maximum of 5 race per day)</h1>
+             <h1 class="text-left">God- 900 Vittles (maximum of 6 race per day)</h1>
+
+             <h1 class="text-yellow-400 mt-5">Note: You need 150 Vittles per Race.</h1>
+         </div>
+     </div>
+     <div class="flex py-2 ">
+        <div class="bg-gray-50 flex flex-col items-start text-white p-2 px-6 rounded-md bg-opacity-20">
+            <h1 class="font-bold text-xl  text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-green-400">Cryptobunnies Vittles will reset every 9:00am (PH) / (1:00am UTC)
+                Rewards per Race:</h1>
+            <h1 class="font-bold mt-3 text-lg">Champ : 3000 Carrots</h1>
+            <h1 class="font-bold text-lg">2nd : 2400 Carrots</h1>
+            <h1 class="font-bold text-lg">3rd : 1500 Carrots</h1>
+            <h1 class="font-bold text-lg">4-9 : 1000 Carrots</h1>
+
+            <div class="mt-4 flex flex-col space-y-3">
+                <span class="underline">Example: Maximum Possible Income in 30 Bunnies
+                    God = 900 Vittiles = 6Race</span>
+              
+            </div>
+            <div class="mt-4 flex flex-col space-y-3">
+                <span>6Race (All Champion) x 3000 Carrot = 18,000 Carrot
+                </span>
+              
+            </div>
+
+            <div class="flex mt-2 flex-col items-start">
+                <span>18,000 Carrot x 30 Bunnies = 540,000 Carrot</span>
+                <span>540,000 Carrot = 5,400 CRB</span>
+                <span>5400 CRB = 540 Peso</span>
+                <span>540 Peso x 30Days = 16,200 Pesos</span>
+                <span>30 Bunnies Capital = 9,000 Pesos</span>
+                <span>2 Weeks ROI.</span>
+            </div>
+
+            <div class="mt-4 flex flex-col space-y-3">
+                <span class="underline">Example: Minimum Possible Income in 30 Bunnies
+                    God = 900 Vittiles = 6Race</span>
+        </div>
+        <div class="mt-4 flex flex-col space-y-3">
+            <span>6Race (All Loss ) x 1000 Carrot = 6,000 Carrot
+            </span>
+          
+        </div>
+        <div class="flex mt-2 flex-col items-start">
+            <span>6,000 Carrot x 30 Bunnies = 180,000 Carrot</span>
+            <span>180,000 Carrot = 1,800 CRB</span>
+            <span>1,800 CRB = 180 Peso</span>
+            <span>180 Peso x 30Days = 5,400 Pesos</span>
+            <span>30 Bunnies Capital = 9,000 Pesos</span>
+            <span>6 Weeks ROI.</span>
+
+    
+            
+        </div>
+        <div class="mt-4 flex flex-col space-y-3">
+            <span class="underline">Example: Minimum Possible Income in 30 Bunnies
+                God = 900 Vittiles = 6Race</span>
+    </div>
+    <div class="flex mt-2 flex-col items-start">
+        <span>God = 150 Vittles = 6Race</span>
+        <span>6Race x 150 Vittles = 900 Vittles</span>
+        <span>900 Vittles x 30 Bunnies = 27,000 Vittles</span>
+        <span>27,000 Vittles = 27 CRB</span>
+        <span>27 CRB = 2.70 Peso</span>
+        <span>2.70 Peso x 30Days = 81 Pesos</span>
+        <span class="mt-3 font-bold">81 Pesos Total Expenses for Vittles in 30 Bunnies in 30 Days.</span>
+       
+
+
+        
+    </div>
+    
+    </div>
+  </div>
+@endsection
